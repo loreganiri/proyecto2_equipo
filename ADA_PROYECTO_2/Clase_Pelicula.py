84dd99ed187153dbbcaf6312a7d7733e5a17b5df
kargbu <kargb@ciencias.unam.mx>
2024-10-07 13:44:33 -0300

    Documentación corregida

--- ADA_PROYECTO_2/Clase_Pelicula.py
+++ ADA_PROYECTO_2/Clase_Pelicula.py
@@ -1,18 +1,18 @@
 # Esta clase se encargará de representar una película con sus atributos, como el nombre.
 
 class Pelicula:
-    tipo = 'Cine'
+    tipo = 'Cine'                                               # Atributo de clase
     
-    def __init__(self, __titulo, anio, genero, __director):
+    def __init__(self, titulo, anio, genero, director):
         self.__titulo   = __titulo                               # Atributo de instancia
-        self.anio       = anio
+        self.__anio     = __anio
         self.genero     = genero
-        self.__director = __director
+        self.director   = director
 
 # Método para mostrar información
     def mostrar_info(self):
         print(f'Nombre de la película: {self.__titulo}, el género es {self.genero}.')
-        print(f'El director fue {self.__director} en el año {self.anio}')  # Imprime la información
+        print(f'El director fue {self.director} en el año {self.__anio}')  # Imprime la información
 
     # Getter con @property
     @property
@@ -21,7 +21,8 @@ class Pelicula:
 # El decorador @property convierte el método titulo en un getter,
 #  permitiendo acceder al atributo __titulo de manera controlada.
     @property
-    def director(self):
+    def anio(self):
+        return self.__anio
     
     # Setter con @nombre.setter
     @titulo.setter
@@ -33,6 +34,13 @@ class Pelicula:
 # El decorador @titulo.setter define un setter para el atributo __titulo,
 #  permitiendo modificar su valor solo si nuevo_titulo es una cadena de texto.
 #  Si no lo es, se lanza un ValueError.
+
+    @anio.setter
+    def anio(self, corregir_anio):
+        if isinstance(corregir_anio, int):
+            self.__anio = corregir_anio
+        else:
+            raise ValueError('El año de la película debe ser un número')
     
 # Uso de la propiedad
 pelicula = Pelicula('Matriz', 1999, 'Ciencia Ficción', 'Lana Wachowski')
@@ -40,6 +48,6 @@ print(pelicula.titulo)  # Llama al getter sin usar paréntesis
 pelicula.titulo = 'Matrix'  # Llama al setter como una asignación directa
 print(pelicula.titulo)  # Accede nuevamente al atributo usando getter}
 
-pelicula = Pelicula('Una familia normal', 2023, 'Drama', 'Per Hanefjord'))
+pelicula = Pelicula('Una familia normal', 2023, 'Drama', 'Per Hanefjord')
 
 mostrar_info.pelicula
