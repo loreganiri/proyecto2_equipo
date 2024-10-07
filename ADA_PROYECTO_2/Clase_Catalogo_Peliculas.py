50cb4e60e16ce498dfb312c3cf6312d3bf1918c2
kargbu <kargb@ciencias.unam.mx>
2024-10-07 20:43:35 -0300

    Creación de la clase Catálogo de Películas

---
+++ ADA_PROYECTO_2/Clase_Catalogo_Peliculas.py
@@ -0,0 +1,7 @@
+# Crear una clase catálogo que gestione los diferentes catálogos de películas
+
+class Catalogo_Peliculas():                                     # El nombre de el catálogo de películas
+    def __init__(self, nombre_catalogo, ruta_catalogo):     # La ruta del archivo donde se va a guardar el catálogo
+        self.nombre_catalogo = nombre_catalogo
+        self.ruta_catalogo   = ruta_catalogo
+def 
